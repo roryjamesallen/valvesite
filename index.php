b557c464e5eec1e3b9514d9297da596e3d63c70b
Rory Allen <rory@hogwild.uk>
2025-12-26 16:27:55 +0000

    add basic files

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	<title>The Valve That Failed</title>
+	<link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+	<p>Oh baby this valve gonna fail!</p>
+    </body>
+</html>
