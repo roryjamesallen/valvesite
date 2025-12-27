ca0ceb5d90c54aca4a6e68f83acdea1262785414
Rory Allen <rory@hogwild.uk>
2025-12-27 17:50:36 +0000

    add intro animation

--- index.php
+++ index.php
@@ -5,6 +5,26 @@
 	<link rel="stylesheet" href="style.css">
     </head>
     <body>
-	<p>Oh baby this valve gonna fail! It's failing!!! Time for this shit to get real</p>
+      <div id="intro-animation"></div>
     </body>
+    <script>
+     function updateElementText(element, text){
+	 element.innerText = text;
+     }
+     function animateText(element, words, speed){
+	 for (let word_index = 0; word_index < words.length; ++word_index) {
+	     setTimeout(updateElementText, (speed * word_index), element, words[word_index]);
+	 }
+     }
+     function introAnimation(){
+	 const div = document.getElementById('intro-animation');
+	 div.style.display = 'flex';
+	 animateText(div, 'the valve that failed'.split(' '), 150);
+	 setTimeout(() => { div.style.display = 'none' }, 650);
+     }
+     
+     window.onload = function() {
+	     introAnimation();
+     };
+    </script>
 </html>
