a3783074f5bb4140dc4960f3d0598388edf730a4
Rory Allen <rory@hogwild.uk>
2026-01-02 12:29:58 +0000

    prevent browser cache

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <!DOCTYPE html>
+<?php
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+?>
 <html>
     <head>
 	<title>The Valve That Failed</title>
